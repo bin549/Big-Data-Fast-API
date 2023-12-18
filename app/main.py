61372c7f99f9a8f02a1eec97a5c859f3da36e0f6
：) <)@mail.bnuz.edu.cn>
2023-12-18 18:42:17 +0800

    first commit

---
+++ app/main.py
@@ -0,0 +1,27 @@
+# api: getAreaList(provinceId) -> areaCode, cityName,
+#      getProvinceList() -> provinceId, provinceName
+#      getSchoolList(selectedCityId) -> schoolId, schoolName,
+#      getSubjectList(schoolId) -> id, subject
+#      getTermList() -> termId, name
+
+# api: getClassList(selectedSchoolId, gradeId) -> classId, className
+#      getStudentList(selectedClassId.value) -> studentId, name
+
+# api: getTeacherList(school_id)
+
+"""statistic: student"""
+# api:
+# statistic:
+#   api1 -> 评价总数, 平均每学期收到评价数, 正面评价, 负面评价, 收到最多的评价前三, 班级平均数
+#   api2 -> 评语统计柱状图
+
+
+"""statistic: teacher"""
+# api:
+# statistic:
+#   api1 -> 评价总数, 平均每学期收到评价数, 正面评价, 负面评价, 收到最多的评价前三, 班级平均数
+#   api2 -> 评语统计柱状图
+# count:
+#   api1 -> teacher
+#   api2 -> 评语统计柱状图
+
